0e77478b623b9093896158332b2fe610f77919e0
Constantine <tmail99@gmail.com>
2025-06-18 16:49:12 +0300

    iter3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,15 +4,23 @@ import (
 	"net/http"
 	handlers "sh42ers/internal/handler"
 	"sh42ers/internal/storage"
+
+	"github.com/go-chi/chi"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	//mux := http.NewServeMux()
+	mux := chi.NewRouter()
 
 	storageInstance := storage.NewURLStorage(make(map[string]string))
 
-	mux.HandleFunc("POST /{$}", handlers.PostHandler(storageInstance))
-	mux.HandleFunc("GET /{id}", handlers.GetHandler(storageInstance))
+	// // Обработчик net/http
+	// mux.HandleFunc("POST /{$}", handlers.PostHandler(storageInstance))
+	// mux.HandleFunc("GET /{id}", handlers.GetHandler(storageInstance))
+
+	// Обработчик chi
+	mux.Post("/", handlers.PostHandler(storageInstance))
+	mux.Get("/{id}", handlers.GetHandler(storageInstance))
 
 	http.ListenAndServe(":8080", mux)
 }
