164ed93ce490fa851bbcbb42d922e4367058de92
Constantine <tmail99@gmail.com>
2025-06-30 17:12:42 +0300

    :

--- internal/http-server/handlers/url/save/save_test.go
+++ internal/http-server/handlers/url/save/save_test.go
@@ -47,6 +47,91 @@ import (
 // 	}
 // }
 
+// Тузовский
+// func TestSaveHandler(t *testing.T) {
+// 	cases := []struct {
+// 		name      string
+// 		alias     string
+// 		url       string
+// 		respError string
+// 		mockError error
+// 	}{
+// 		{
+// 			name:  "Success",
+// 			alias: "test_alias",
+// 			url:   "https://google.com",
+// 		},
+// 		{
+// 			name:  "Empty alias",
+// 			alias: "",
+// 			url:   "https://google.com",
+// 		},
+// 		{
+// 			name:      "Empty URL",
+// 			url:       "",
+// 			alias:     "some_alias",
+// 			respError: "field URL is a required field",
+// 		},
+// 		{
+// 			name:      "Invalid URL",
+// 			url:       "some invalid URL",
+// 			alias:     "some_alias",
+// 			respError: "field URL is not a valid URL",
+// 		},
+// 		{
+// 			name:      "SaveURL Error",
+// 			alias:     "test_alias",
+// 			url:       "https://google.com",
+// 			respError: "failed to add url",
+// 			mockError: errors.New("unexpected error"),
+// 		},
+// 	}
+
+// 	for _, tc := range cases {
+// 		tc := tc
+
+// 		t.Run(tc.name, func(t *testing.T) {
+// 			t.Parallel()
+
+// 			urlSaverMock := mocks.NewURLSaver(t)
+
+// 			if tc.respError == "" || tc.mockError != nil {
+// 				urlSaverMock.On("SaveURL", tc.url, mock.AnythingOfType("string")).
+// 					Return(int64(1), tc.mockError).
+// 					Once()
+// 			}
+
+// 			handler := save.New(slogdiscard.NewDiscardLogger(), urlSaverMock)
+
+// 			input := fmt.Sprintf(`{"url": "%s", "alias": "%s"}`, tc.url, tc.alias)
+
+// 			req, err := http.NewRequest(http.MethodPost, "/save", bytes.NewReader([]byte(input)))
+// 			// NoError проверяет, что функция не вернула ошибку.
+// 			require.NoError(t, err)
+
+// 			rr := httptest.NewRecorder()
+// 			handler.ServeHTTP(rr, req)
+
+// 			//Equal производит сравнение двух значений
+// 			require.Equal(t, rr.Code, http.StatusOK)
+
+// 			body := rr.Body.String()
+
+// 			var resp save.Response
+// 			// NoError проверяет, что функция не вернула ошибку.
+// 			require.NoError(t, json.Unmarshal([]byte(body), &resp))
+// 			//Equal производит сравнение двух значений
+// 			require.Equal(t, tc.respError, resp.Error)
+
+// 			// TODO: add more checks
+// 		})
+// 	}
+// }
+
+// Я проверяю только правильность методов.
+// И главная ерунда у меня- args{mapstorage.NewURLStorage(make(map[string]string))}
+// Прямое обращение к другому участку кода,
+// который я сам хочу заменить (на sqlite)
 func TestSaveNew(t *testing.T) {
 	type args struct {
 		urlSaver URLSaver
