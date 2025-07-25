e6a79e17654eaeeea2343050d36465524a14e31e
Constantine <tmail99@gmail.com>
2025-07-25 09:23:00 +0300

    --

--- internal/http-server/handlers/url/textsave/savetext.go
+++ internal/http-server/handlers/url/textsave/savetext.go
@@ -7,8 +7,6 @@ import (
 	"net/http"
 	"sh42ers/internal/config"
 	"sh42ers/internal/lib/random"
-
-	"strings"
 )
 
 // // До go generate нужно установить библиотеку
@@ -31,58 +29,59 @@ type URLtextSaver interface {
 func New(log *slog.Logger, urlSaver URLtextSaver) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
-			contentType := r.Header.Get("Content-Type")
-			if strings.Contains(contentType, "text/plain") {
-				// //gzip
-				// reader, err := getGzipReader(r)
-				// if err != nil {
-				// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-				// 	return
-				// }
+			// contentType := r.Header.Get("Content-Type")
+			// if strings.Contains(contentType, "text/plain") {
 
-				// body, err := io.ReadAll(reader)
-				// //
+			// //gzip
+			// reader, err := getGzipReader(r)
+			// if err != nil {
+			// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+			// 	return
+			// }
 
-				body, err := io.ReadAll(r.Body)
-				if err != nil {
-					http.Error(w, "Failed to read request body", http.StatusBadRequest)
-					return
-				}
-				defer r.Body.Close()
+			// body, err := io.ReadAll(reader)
+			// //
 
-				// Преобразуем тело запроса (тип []byte) в строку:
-				url := string(body)
-
-				// // Генерируем короткий идентификатор и создаем запись в нашем хранилище
-				// //config.FlagURL соответствует "http://" + req.Host если не использовать аргументы
-				alias := random.NewRandomString(config.AliasLength)
-
-				//// ЗДЕСЬ СПОТЫКАЕТСЯ mock на проверке, когда все в порядке.
-				//// "Плохой" метод сюда не заходит!
-
-				// Объект urlSaver (переданный при создании хендлера из main)
-				// используется именно тут!
-				err = urlSaver.SaveURL(url, alias)
-				//if urlSaver.SaveURL(url, alias) != nil {
-				if err != nil {
-					fmt.Println("failed to add url")
-					return
-				}
-
-				log.Info("url added", slog.String("alias", alias))
-
-				// Устанавливаем статус ответа 201
-				w.WriteHeader(http.StatusCreated)
-				// fmt.Fprint(w, config.FlagURL+"/"+alias)
-				if config.FlagURL == "none" {
-					fmt.Fprint(w, "http://"+r.Host+"/"+alias)
-				} else {
-					fmt.Fprint(w, config.FlagURL+"/"+alias)
-				}
-
-			} else {
-				http.Error(w, "Incorrect Content-Type. Expected text/plain", http.StatusUnauthorized) //http.StatusBadRequest)
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Failed to read request body", http.StatusBadRequest)
+				return
 			}
+			defer r.Body.Close()
+
+			// Преобразуем тело запроса (тип []byte) в строку:
+			url := string(body)
+
+			// // Генерируем короткий идентификатор и создаем запись в нашем хранилище
+			// //config.FlagURL соответствует "http://" + req.Host если не использовать аргументы
+			alias := random.NewRandomString(config.AliasLength)
+
+			//// ЗДЕСЬ СПОТЫКАЕТСЯ mock на проверке, когда все в порядке.
+			//// "Плохой" метод сюда не заходит!
+
+			// Объект urlSaver (переданный при создании хендлера из main)
+			// используется именно тут!
+			err = urlSaver.SaveURL(url, alias)
+			//if urlSaver.SaveURL(url, alias) != nil {
+			if err != nil {
+				fmt.Println("failed to add url")
+				return
+			}
+
+			log.Info("url added", slog.String("alias", alias))
+
+			// Устанавливаем статус ответа 201
+			w.WriteHeader(http.StatusCreated)
+			// fmt.Fprint(w, config.FlagURL+"/"+alias)
+			if config.FlagURL == "none" {
+				fmt.Fprint(w, "http://"+r.Host+"/"+alias)
+			} else {
+				fmt.Fprint(w, config.FlagURL+"/"+alias)
+			}
+
+			// } else {
+			// 	http.Error(w, "Incorrect Content-Type. Expected text/plain", http.StatusUnauthorized) //http.StatusBadRequest)
+			// }
 		} else {
 			http.Error(w, "Method not allowed", http.StatusBadRequest)
 		}
